003efe5f7ac590801cb9f78c356e4ac4ee05b21c
Mahmoud <20812017101210@eng.zu.edu.eg>
2024-09-07 20:09:34 +0300

    update functions

--- src/app/Services/store/products.service.ts
+++ src/app/Services/store/products.service.ts
@@ -5,33 +5,35 @@ import { Product } from '../../Models/product';
 
 @Injectable()
 export class ProductsService {
-  apiUrl = 'https://localhost:7082/api/Products/';
+  ProductsURL = 'https://localhost:7082/api/Products/';
   private searchTerm = new BehaviorSubject<string>('');
   currentSearchTerm = this.searchTerm.asObservable();
+  token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik1haG1vdWRAZ21haWwuY29tIiwiZ2l2ZW5fbmFtZSI6Ik1haG1vdWQiLCJuYmYiOjE3MjU3MDM4NzksImV4cCI6MTcyNTc5MDI3OSwiaWF0IjoxNzI1NzAzODc5LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDgyIn0.lQ33BtBHxx8uTSQtDNGrN3x5ZrgItIojIiPgCkdDELA';
 
   constructor(private http: HttpClient) {}
+
 
   updateSearchTerm(term: string) {
     this.searchTerm.next(term);
   }
-
+  // GetAll Methods
   getAllProducts(): Observable<Product[]> {
-    const token =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik1haG1vdWRAZ21haWwuY29tIiwiZ2l2ZW5fbmFtZSI6Ik1haG1vdWQiLCJuYmYiOjE3MjU3MDM4NzksImV4cCI6MTcyNTc5MDI3OSwiaWF0IjoxNzI1NzAzODc5LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDgyIn0.lQ33BtBHxx8uTSQtDNGrN3x5ZrgItIojIiPgCkdDELA';
-
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`,
-    });
-    return this.http.get<Product[]>(this.apiUrl + 'GetProducts', { headers });
+    const headers = new HttpHeaders({Authorization: `Bearer ${this.token}`});
+    return this.http.get<Product[]>(this.ProductsURL + 'GetProducts', { headers });
   }
+  getAllBrands(): Observable<any[]> {
+    const headers = new HttpHeaders({Authorization: `Bearer ${this.token}`});
+    return this.http.get<Product[]>(this.ProductsURL + 'GetProductBrands/Brands', { headers });
+  }
+  getAllSubCategories():Observable<any[]> {
+    const headers = new HttpHeaders({Authorization: `Bearer ${this.token}`});
+    return this.http.get<Product[]>(this.ProductsURL + 'GetProductSubCategories/Types', { headers });
+  };
 
-  // getAllBrands(): Observable<any[]> {
-  //   const token =
-  //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ik1haG1vdWRAZ21haWwuY29tIiwiZ2l2ZW5fbmFtZSI6Ik1haG1vdWQiLCJuYmYiOjE3MjU3MDM4NzksImV4cCI6MTcyNTc5MDI3OSwiaWF0IjoxNzI1NzAzODc5LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDgyIn0.lQ33BtBHxx8uTSQtDNGrN3x5ZrgItIojIiPgCkdDELA';
-
-  //   const headers = new HttpHeaders({
-  //     Authorization: `Bearer ${token}`,
-  //   });
-  //   return this.http.get<Product[]>(this.apiUrl2, { headers });
-  // }
+  //Get Produc By Id
+  getProductbyId(id:number):Observable<Product>{
+    const headers = new HttpHeaders({Authorization: `Bearer ${this.token}`});
+    return this.http.get<Product>(this.ProductsURL + 'GetProductById/' + id , { headers });
+  }
 }
